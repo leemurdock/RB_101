80f6d00a026d735161cd64f173a34bc8541b0967
Lee Murdock <leemurdock@pm.me>
2023-08-23 16:14:18 -0500

    RB101 Easy3 problems + .DS_Store

---
+++ Easy3/E2.rb
@@ -0,0 +1,12 @@
+
+puts "==> Enter a number:"
+num1 = gets.chomp.to_i
+puts "==> Enter a 2nd number:"
+num2 = gets.chomp.to_i
+puts "==> #{num1} + #{num2} = #{num1 + num2}"
+puts "==> #{num1} - #{num2} = #{num1 - num2}"
+puts "==> #{num1} * #{num2} = #{num1 * num2}"
+puts "==> #{num1} / #{num2} = #{num1 / num2}"
+puts "==> #{num1} % #{num2} = #{num1 % num2}"
+puts "==> #{num1} ** #{num2} = #{num1 ** num2}"
+
---
+++ Easy3/E5.rb
@@ -0,0 +1,9 @@
+def multiply(x, y)
+	x * y
+end
+
+def square(n)
+	multiply(n, n)
+end
+
+puts square(3)
