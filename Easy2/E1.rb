1e5f90a13cdd02e7827b3f9229b1538b0e049e80
Lee Murdock <leemurdock@pm.me>
2023-08-23 21:14:19 -0500

    Delete Easy2 directory

--- Easy2/E1.rb
+++
@@ -1,9 +0,0 @@
-puts "What's your name?"
-name = gets.chomp
-name = 'Teddy' if name.empty?
-
-age = rand(20..200)
-puts ("#{name} is #{age} years old!")
-
-#gets always gives us a string
-#puts always returns nil
